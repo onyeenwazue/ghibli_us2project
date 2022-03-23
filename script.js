00951468df643277ee70b8526937617849b25d94
Onyee Nwazue <f6nwazue@gmail.com>
2022-03-23 16:21:19 -0700

    comments of what is required to make console info show up

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.querySelector('button')
 const list = document.querySelector('ul')
 
+
+// Clicking on the button will generate a random Ghibli movie and show the Title, Description and Image
 button.addEventListener('click', async () => {
   let response = await axios.get("https://ghibliapi.herokuapp.com/films")
 // console.log(response)
@@ -8,3 +10,4 @@ let ghibliResponse = response.data
 let i = ghibliResponse[Math.floor(Math.random() * ghibliResponse.length)]
 console.log(i)
 })
+
