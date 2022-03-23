2564fd19ca293f70e3c80dc8a508f5d6b5608654
Onyee Nwazue <f6nwazue@gmail.com>
2022-03-23 16:45:34 -0700

    title, release and description show on page

--- script.js
+++ script.js
@@ -8,6 +8,28 @@ button.addEventListener('click', async () => {
 // console.log(response)
 let ghibliResponse = response.data
 let i = ghibliResponse[Math.floor(Math.random() * ghibliResponse.length)]
-console.log(i)
+// console.log(i)
+let title = i.title
+let release = i.release_date
+let image = i.image
+let description = i.description
+console.log(title)
+console.log(release)
+console.log(image)
+console.log(description)
+
+document.querySelector(`h1`).innerText = title
+document.querySelector('h2').innerText = release
+let pic = document.createElement(`img`)
+pic.src = image
+document.querySelector(`h3`).innerText = description
+
+
+// let info = document.createElement(`h1`)
+// let container = document.querySelector(`.title`)
+// container.appendChild(info)
+//create an element and save it to the variable
+//chnage the innerText to be desired value
+//append created element to the page
 })
 
